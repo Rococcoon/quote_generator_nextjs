052041b4637e489f526a509f530b3d1756146f1b
Rococcoon <spacedangles@gmail.com>
2025-09-26 16:33:31 -0700

    tidy: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import styles from "./page.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.page}>
+      <h1>WELCOME</h1>
+      <div className={styles.linkContainer}>
+        <Link className={styles.linkCard} href={"/quotes"}>QUOTES</Link>
+        <Link className={styles.linkCard} href={"/search"}>SEARCH</Link>
+      </div>
     </main>
   );
 }
---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+import BookList from "@/components/bookList/bookList";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <BookList />
+    </main>
+  );
+}
